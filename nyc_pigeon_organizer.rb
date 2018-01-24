0d66de66e510aafbf18326acde7daf6c134aa12d
Melissa Nock <melnock@gmail.com>
2018-01-24 14:13:51 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
+
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeons_name = []
+  pigeons = {}
+  data.each{|sym, hash|
+    hash.each{|k, arr|
+      arr.each_index{|x|
+        pigeons_name << arr[x]
+      }
+    }
+  }
+  pigeons_name.uniq!
+  pigeons_name.each{|x| 
+    pigeons[x] = {}
+  }
+  pigeons
+ 
 end
