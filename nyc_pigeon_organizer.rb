68e559d34ab07a72983d3769ca4f1896b1bfe41a
Melissa Nock <melnock@gmail.com>
2018-01-24 14:29:33 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,28 @@
+
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeons_name = []
+  pigeons = {}
+  data.each{|sym, hash|
+    hash.each{|k, arr|
+      arr.each_index{|x|
+        pigeons_name << arr[x]
+      }
+    }
+  }
+  pigeons_name.uniq!
+  pigeons_name.each{|x| 
+    pigeons[x] = {}
+    data.each {|sym, hash|
+      pigeons[x][sym] = []
+      hash.each{|k, arr|
+        if hash[k].include?(x)
+          pigeons[x][sym] << k.to_s
+        end 
+      }
+    }
+  }
+  pigeons
+
 end
